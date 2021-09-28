c9f043028c72aa07faef30bd10641795bc4879a3
MediaWork5 <mohamed.hasibate370.50@gmail.com>
2021-09-28 19:02:40 +0200

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <div className='header'>
-            <Link to='/'>
+            <Link to='/MovieApp/'>
                 <div className='logo'>Movie App</div>
             </Link>
             <div className='user-image'>
